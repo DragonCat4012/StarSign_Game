937a018da94ead9826c0523436bc49807f1c70a5
kiara <sophie_natalie.zieschang@mailbox.tu-dresden.de>
2024-06-22 13:16:31 +0200

    Connect Keybindings Scene

--- Scenes/Menu/MainMenu.gd
+++ Scenes/Menu/MainMenu.gd
@@ -3,10 +3,12 @@ extends Node2D
 @onready var PlayButton := $VBoxContainer/PlayButton
 @onready var OptionsButton := $VBoxContainer/OptionsButton
 @onready var ExitButton := $VBoxContainer/ExitButton
+@onready var KeybindingsButton := $VBoxContainer/KeybindingsButton
 
 func _ready():
 	PlayButton.pressed.connect(self._play_button_pressed.bind())
 	OptionsButton.pressed.connect(self._options_button_pressed.bind())
+	KeybindingsButton.pressed.connect(self._keybindings_button_pressed.bind())
 	ExitButton.pressed.connect(self._exit_button_pressed.bind())
 		
 func _play_button_pressed():
@@ -14,6 +16,9 @@ func _play_button_pressed():
 	
 func _options_button_pressed():
 	print("TODO: Button Options pressed")
+	
+func _keybindings_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu/Keybindings.tscn")
 	
 func _exit_button_pressed():
 	get_tree().quit()
