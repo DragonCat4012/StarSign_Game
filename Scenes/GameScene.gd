2c55149b2758104429454ed1b07610e4efe132bf
Kiara <frida.shivaa@gmail.com>
2024-07-02 22:37:22 +0200

    Update Lifght energy for night/day

--- Scenes/GameScene.gd
+++ Scenes/GameScene.gd
@@ -1,6 +1,7 @@
 extends Node3D
 @onready var compass = $UIOverlay/Compass
 @onready var needle = $UIOverlay/Clock/Needle
+@onready var lightSource = $DirectionalLight3D
 
 func _physics_process(delta):
 	if $Player.position.y < -75:
@@ -12,5 +13,10 @@ func _process(delta: float):
 		GameManager._toggle_weapon()
 		
 	compass._updateTexture($Player.compassDirection)
-	$DirectionalLight3D.rotate_x(deg_to_rad(0.1))
+	lightSource.rotate_x(deg_to_rad(0.5))
+
+	if lightSource.rotation_degrees.x > 0: # Night
+		lightSource.light_energy = 0.01
+	else:
+		lightSource.light_energy = 1.0 # Day
 	needle.rotation_degrees += 0.1
