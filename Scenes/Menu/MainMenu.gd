6388f21d61ee2027a49e53e3da64d901aa68e30e
Kiara <frida.shivaa@gmail.com>
2024-07-07 17:28:14 +0200

    add Verion in Menu

--- Scenes/Menu/MainMenu.gd
+++ Scenes/Menu/MainMenu.gd
@@ -4,12 +4,19 @@ extends Node2D
 @onready var OptionsButton := $VBoxContainer/OptionsButton
 @onready var ExitButton := $VBoxContainer/ExitButton
 @onready var KeybindingsButton := $VBoxContainer/KeybindingsButton
+@onready var verison_label = $VBoxContainer/VerisonLabel
 
 func _ready():
 	PlayButton.pressed.connect(self._play_button_pressed.bind())
 	OptionsButton.pressed.connect(self._options_button_pressed.bind())
 	KeybindingsButton.pressed.connect(self._keybindings_button_pressed.bind())
 	ExitButton.pressed.connect(self._exit_button_pressed.bind())
+	
+	var preset = ConfigFile.new()
+	preset.load("res://export_presets.cfg")
+	var version = preset.get_value("preset.0.options", "application/file_version")
+	var build =  preset.get_value("preset.0.options", "application/product_version")
+	verison_label.text = version + " (" + build + ")"
 		
 func _play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/GameScene.tscn")
