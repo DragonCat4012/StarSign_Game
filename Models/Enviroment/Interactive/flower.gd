0803fff88b503c0f933d81d1387f52721417619c
Kiara <frida.shivaa@gmail.com>
2024-08-22 15:19:51 +0200

    Add foliage

--- Models/Enviroment/Interactive/flower.gd
+++ Models/Enviroment/Interactive/flower.gd
@@ -3,15 +3,21 @@ extends Node3D
 @onready var gpu_particles_3d = $GPUParticles3D
 @onready var flower_waves = $FlowerWaves
 
-
+func _ready():
+	gpu_particles_3d.emitting = false
+	flower_waves.get_active_material(0).set_shader_parameter("speed", Vector2(0.0,0.0))
+	flower_waves.visible = false
+	
 func _on_trigger_body_entered(body):
 	if body.name == "Player":
 		gpu_particles_3d.emitting = true
 		animation_player.play("fade_in")
+		flower_waves.visible = true
 		flower_waves.get_active_material(0).set_shader_parameter("speed", Vector2(-0.1,-0.1))
 		
 func _on_trigger_body_exited(body):
 	if body.name == "Player":
 		animation_player.play("fade_out")
 		gpu_particles_3d.emitting = false
+		flower_waves.visible = false
 		flower_waves.get_active_material(0).set_shader_parameter("speed", Vector2(0.0,0.0))
