9d245ef97a12f93a1c70bf60009268f0d7255d99
Kiara <frida.shivaa@gmail.com>
2024-08-24 15:54:10 +0200

    fix Fire

--- Models/Enviroment/Fire/fire.gd
+++ Models/Enviroment/Fire/fire.gd
@@ -4,15 +4,15 @@ extends Node3D
 @onready var fire = $Fire
 
 func _ready():
-	EventSystem.PauseEnded.connect(pause_particles)
-	EventSystem.PauseStared.connect(_start_particles)
+	EventSystem.PauseEnded.connect(_start_particles)
+	EventSystem.PauseStared.connect(_freeze_particles_particles)
 	
-func _start_particles():
+func _freeze_particles_particles():
 	fire.material_override.set_shader_parameter("FireActivity", 0.0)
 	smoke.speed_scale = 0
 	amber.speed_scale = 0
 
-func pause_particles():
+func _start_particles():
 	fire.material_override.set_shader_parameter("FireActivity", 1.0)
 	smoke.speed_scale = 1
 	amber.speed_scale = 1
