1e5b35383753c21fcddaafa1bf30756ff7538735
Kiara <frida.shivaa@gmail.com>
2024-08-25 19:42:04 +0200

    Feature: Stairs

--- Models/Player.gd
+++ Models/Player.gd
@@ -34,6 +34,11 @@ var isEmoteAnimationPlaying = false
 # Compass
 var compassDirection = 0
 
+# Stairs
+const MAX_STEP_HEIGHT = 1
+var _snapped_to_stairs_last_frame := false
+var _last_frame_was_on_floor = -INF
+
 func _ready():
 	get_weapon_children(true) # TODO: load from save file
 	add_child(timer)
@@ -63,13 +68,15 @@ func _unhandled_input(event: InputEvent):
 			pitch_input = - event.relative.y * mouse_sensitivity
 
 func _physics_process(delta):
+	if is_on_floor(): _last_frame_was_on_floor = Engine.get_physics_frames()
+	
 	isJumping = !is_on_floor()
 	if $AnimationPlayer.current_animation == emoteAnimations[emoteIndex]: # dont move during emote
 		return 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if is_on_floor():
+	if is_on_floor() or _snapped_to_stairs_last_frame:
 		jump_count = 0
 		if Input.is_action_just_pressed("Attack1"):
 			$AnimationPlayer.play(_select_attack_1_animation())
@@ -104,7 +111,10 @@ func _physics_process(delta):
 	animation_tree.set("parameters/conditions/walking", input_dir != Vector2.ZERO && is_on_floor())
 	animation_tree.set("parameters/conditions/jumping", !is_on_floor())
 	animation_tree.set("parameters/conditions/landing", is_on_floor())
-	move_and_slide()
+	
+	if not _snap_up_stairs_check(delta):
+		move_and_slide()
+		_snap_down_to_stairs_check()
 
 # Jump animations
 func _jump_particles():
@@ -157,3 +167,45 @@ func _display_emotion() -> String:
 
 func _finished_emotion_animation():
 	isEmoteAnimationPlaying = false
+
+# Stairs
+func is_surface_too_steep(normal: Vector3) -> bool:
+	return normal.angle_to(Vector3.UP) > self.floor_max_angle
+
+func _run_body_test_motion(from: Transform3D, motion: Vector3, result = null) -> bool:
+	if not result: result = PhysicsTestMotionResult3D.new()
+	var params = PhysicsTestMotionParameters3D.new()
+	params.from = from
+	params.motion = motion
+	return PhysicsServer3D.body_test_motion(self.get_rid(), params, result)
+
+func _snap_down_to_stairs_check():
+	var did_snap := false
+	var floor_below: bool = %StairsBelowRayCast.is_colliding() and not is_surface_too_steep(%StairsBelowRayCast.get_collision_normal())
+	var was_on_floor_last_frame = Engine.get_physics_frames() - _last_frame_was_on_floor == 1
+	if not is_on_floor() and velocity.y <= 0 and (was_on_floor_last_frame or _snapped_to_stairs_last_frame) and floor_below:
+		var body_test_result = PhysicsTestMotionResult3D.new()
+		if _run_body_test_motion(self.global_transform, Vector3(0, -MAX_STEP_HEIGHT, 0), body_test_result):
+			var translate_y = body_test_result.get_travel().y
+			self.position.y += translate_y
+			apply_floor_snap()
+			did_snap = true
+	_snapped_to_stairs_last_frame = did_snap
+
+func _snap_up_stairs_check(delta) -> bool:
+	if not is_on_floor() and not _snapped_to_stairs_last_frame: return false
+	var expected_move_motion = self.velocity * Vector3(1,0,1)*delta
+	var step_pos_with_clearance = self.global_transform.translated(expected_move_motion + Vector3(0, MAX_STEP_HEIGHT*2, 0))
+	var down_check_result = PhysicsTestMotionResult3D.new()
+	if(_run_body_test_motion(step_pos_with_clearance, Vector3(0, -MAX_STEP_HEIGHT*2, 0), down_check_result) 
+	and (down_check_result.get_collider().is_class("StaticBody3d") or down_check_result.get_collider().is_class("CGShape3D"))):
+		var step_height = ((step_pos_with_clearance.origin + down_check_result.get_travel()) - self.global_position).y
+		if step_height > MAX_STEP_HEIGHT or step_height <= 0.01 or (down_check_result.get_collision_point() - self.global_position).y > MAX_STEP_HEIGHT: return false
+		%StairsAheadRayCast.global_position = down_check_result.get_collision_point() + Vector3(0,MAX_STEP_HEIGHT,0) + expected_move_motion.normalized()*0.1
+		%StairsAheadRayCast.force_raycast_update()
+		if %StairsAheadRayCast.is_colliding() and not is_surface_too_steep(%StairsAheadRayCast.get_collision_normal()):
+			self.global_position = step_pos_with_clearance.origin + down_check_result.get_travel()
+			apply_floor_snap()
+			_snapped_to_stairs_last_frame = true
+			return true
+	return false
